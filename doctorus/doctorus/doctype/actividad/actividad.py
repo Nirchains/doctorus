0eb5d277bd076f338cab31ba02affe78c15e63bc
Nirchains <nirchains@gmail.com>
2019-05-07 08:55:10 +0200

    corrección de estados de actividades

--- doctorus/doctorus/doctype/actividad/actividad.py
+++ doctorus/doctorus/doctype/actividad/actividad.py
@@ -19,18 +19,24 @@ class Actividad(Document):
 
 	
 def validate_requirements(expedient):
+	#print("validando expediente: {0}".format(expedient))
+
 	activities = 'No superadas'
 	if check_requirements(expedient):
+		#print("Actividades superadas")
 		activities = 'Superadas'
 
 	doc_expedient = frappe.get_doc("Expediente", expedient)
 
 	if doc_expedient.activities != activities:
+		#print("Actualizando documento")
 		doc_expedient.activities = activities
 		doc_expedient.save()
 		frappe.clear_cache()
+		#frappe.db.set_value('Expediente', expedient, 'activities', activities)
 			
 def check_requirements(expedient):
+	#print("validando expediente: {0}".format(expedient))
 	rules = frappe.db.get_list('Requisitos Actividades', filters={
 	'parent': 'Configuracion del Programa',
 	'parentfield': 't_activity_requirements'
@@ -40,10 +46,20 @@ def check_requirements(expedient):
 
 	for r in rules:
 		count = int(frappe.db.sql("""select count(*) from `tabActividad` where 
-			expedient=%(expedient)s and docstatus = 1 and workflow_state = 'aprobado' 
+			expedient=%(expedient)s and docstatus = 1
 			and activity_type=%(activity_type)s """, {"expedient": expedient, "activity_type": r.activity_type})[0][0])
 		
+		#frappe.log_error("Contador de expediente '{0}' y actividad '{1}': {2}".format(expedient, r.activity_type, count))
+		#print("Contador de expediente '{0}' y actividad '{1}': {2}".format(expedient, r.activity_type, count))
 		if count < r.min:
 			meet_requirements = False
 
 	return meet_requirements
+
+def update_all_requirements():
+	expedientes = frappe.db.get_list('Expediente')
+
+	for expediente in expedientes:
+		validate_requirements(expediente.name)
+
+	frappe.db.commit()
